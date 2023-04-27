b318d165c95b653a7b812504508d9dcd8ddcea5e
Deyan Diulgerov <chukstv@abv.bg>
2023-04-27 18:44:56 +0300

    Add files via upload

---
+++ 01. Food Finderr/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _01.Food_Finderr
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var vowels = Console.ReadLine().Split().Select(char.Parse).ToArray();
+            var consonants = Console.ReadLine().Split().Select(char.Parse).ToArray();
+
+
+            var queueVowels = new Queue<char>(vowels);
+            var stackConsonants = new Stack<char>(consonants);
+
+            var foodFinder = new Dictionary<string, int>()
+            {
+                {"pear", 0},
+                {"flour", 0},
+                {"pork", 0},
+                {"olive", 0}
+            };
+
+            var pear = "pear".ToCharArray().ToList();
+            var flour = "flour".ToCharArray().ToList();
+            var pork = "pork".ToCharArray().ToList();
+            var olive = "olive".ToCharArray().ToList();
+
+
+
+            while (queueVowels.Count > 0 && stackConsonants.Count > 0)
+            {
+                var currentVowel = queueVowels.Dequeue();
+                queueVowels.Enqueue(currentVowel);
+                var currentConsonant = stackConsonants.Pop();
+
+
+                if (pear.Contains(currentVowel))
+                {
+                    pear.Remove(currentVowel);
+                }
+                if(pear.Contains(currentConsonant))
+                {
+                    pear.Remove(currentConsonant);
+                }
+                if (flour.Contains(currentVowel))
+                {
+                    flour.Remove(currentVowel);
+                }
+                if (flour.Contains(currentConsonant))
+                {
+                    flour.Remove(currentConsonant);
+                }
+                if (pork.Contains(currentVowel))
+                {
+                    pork.Remove(currentVowel);
+                }
+                if (pork.Contains(currentConsonant))
+                {
+                    pork.Remove(currentConsonant);
+                }
+                if (olive.Contains(currentVowel))
+                {
+                    olive.Remove(currentVowel);
+                }
+                if (olive.Contains(currentConsonant))
+                {
+                    olive.Remove(currentConsonant);
+                }
+
+                if(pear.Count == 0)
+                {
+                    foodFinder["pear"]++;
+                    pear = "pear".ToCharArray().ToList();
+                }
+                if (flour.Count == 0)
+                {
+                    foodFinder["flour"]++;
+                    flour = "flour".ToCharArray().ToList();
+                }
+                if (pork.Count == 0)
+                {
+                    foodFinder["pork"]++;
+                    pork = "pork".ToCharArray().ToList();
+                }
+                if (olive.Count == 0)
+                {
+                    foodFinder["olive"]++;
+                    olive = "olive".ToCharArray().ToList();
+                }
+            }
+            Console.WriteLine($"Words found: {foodFinder.Sum(x => x.Value)}");
+
+            foreach (var kvp in foodFinder.Where(x => x.Value > 0).ToDictionary(x => x.Key, x => x.Value))
+             {
+                 if (kvp.Value > 0)
+                 {
+                     Console.WriteLine($"{kvp.Key}");
+                 }                  
+             }
+
+        }
+    }
+}
---
+++ 01. Meal Plan/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Meal_Plan
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var mealsCalories = Console.ReadLine().Split().ToArray();
+            var caloriesAllowedPerDay = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var queueMeals = new Queue<string>(mealsCalories);
+            var stackCalories = new Stack<int>(caloriesAllowedPerDay);
+
+            var numberOfMeals = 0;
+
+
+            while (queueMeals.Count > 0 && stackCalories.Count > 0)
+            {
+                var mealCalories = 0;
+                var currentMeal = queueMeals.Peek();          
+                var currentAllowedCalories = stackCalories.Peek();
+
+                if (currentMeal == "salad")
+                {
+                    mealCalories = 350;
+                }
+                else if (currentMeal == "soup")
+                {
+                    mealCalories = 490;
+                }
+                else if (currentMeal == "pasta")
+                {
+                    mealCalories = 680;
+                }
+                else if (currentMeal == "steak")
+                {
+                    mealCalories = 790;
+                }               
+
+                var newCalories = currentAllowedCalories - mealCalories;
+                if(newCalories > 0)
+                {
+                    queueMeals.Dequeue();
+                    stackCalories.Pop();
+                    stackCalories.Push(newCalories);
+                    numberOfMeals++;
+                }
+                else if (newCalories == 0)
+                {
+                    stackCalories.Pop();
+                    continue;
+                }
+                else if (newCalories < 0)
+                {
+                    var leftOverFood = newCalories;
+                    stackCalories.Pop();
+                    queueMeals.Dequeue();
+                    numberOfMeals++;
+                    if (stackCalories.Count > 0)
+                    {
+                        var food = stackCalories.Peek();
+
+                        var newDay = stackCalories.Peek() + leftOverFood;
+                        /*queueMeals.Dequeue();
+                        numberOfMeals++;*/
+                        stackCalories.Pop();
+                        stackCalories.Push(newDay);
+                    }
+                }
+            }
+
+            if(queueMeals.Count == 0)
+            {
+                Console.WriteLine($"John had {numberOfMeals} meals.");
+                Console.WriteLine($"For the next few days, he can eat {String.Join(", ", stackCalories)} calories.");
+            }
+            else if(queueMeals.Count > 0)
+            {
+                Console.WriteLine($"John ate enough, he had {numberOfMeals} meals.");
+                Console.WriteLine($"Meals left: {String.Join(", ", queueMeals)}.");
+            }
+        }
+    }
+}
